14ef3c948c5dd37c50a57781eef2faa8dcea2e61
prettylittlebaby <144493425+lgnaboya@users.noreply.github.com>
2025-10-23 07:38:03 +0800

    updated finale

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
-import react from "@vitejs/plugin-react-swc";
-import { componentTagger } from "lovable-tagger";
-import path from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // 👈 IMPORTANT for GitHub Pages (root-level repo)
+  base: "/ln-portfolio/", // 👈 update this for GitHub Pages subpath
 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
